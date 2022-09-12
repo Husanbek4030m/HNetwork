cb669e5a4989bcbc464d1e71dbaa4f626ca94535
husanbek <husanbek4030m@gmail.com>
2022-09-12 16:27:08 +0500

    Initial Commit

---
+++ HNetwork/HNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HNetworkApp.swift
+//  HNetwork
+//
+//  Created by Karavella on 12/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
